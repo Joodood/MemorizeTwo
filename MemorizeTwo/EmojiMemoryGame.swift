a233b74e0e6d1a5ead8a020c108992306cb38e99
Joodood <jude.hathway@gmail.com>
2023-11-28 13:43:22 -0500

    Added model, modelview

---
+++ MemorizeTwo/EmojiMemoryGame.swift
@@ -0,0 +1,8 @@
+//
+//  EmojiMemoryGame.swift
+//  MemorizeTwo
+//
+//  Created by Jude Hathway on 11/25/23.
+//
+
+import Foundation
---
+++ MemorizeTwo/MemorizeGame.swift
@@ -0,0 +1,8 @@
+//
+//  MemorizeGame.swift
+//  MemorizeTwo
+//
+//  Created by Jude Hathway on 11/25/23.
+//
+
+import Foundation
